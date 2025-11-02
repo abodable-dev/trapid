796e6816300040641d388370be1e13ee40b74897
Abodable Developers <115595308+abodable-dev@users.noreply.github.com>
2025-11-02 13:44:07 +1000

    Fix import path in ImportProgress component

--- frontend/src/components/imports/ImportProgress.jsx
+++ frontend/src/components/imports/ImportProgress.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from 'react'
-import api from '../../utils/api'
+import { api } from '../../api'
 
 export default function ImportProgress({ sessionKey, onComplete }) {
   const [status, setStatus] = useState('queued')
