ed75d1b8e3e3d70c580aec5dfc96b0755d56a806
Robert Harder <rob@Rachels-MacBook-Pro-2.local>
2025-11-10 13:38:38 +1000

    Fix Solid Queue recurring tasks configuration

--- config/initializers/solid_queue.rb
+++
@@ -1,9 +0,0 @@
-Rails.application.configure do
-  config.solid_queue.recurring_tasks = [
-    {
-      key: "apply_price_updates",
-      class_name: "ApplyPriceUpdatesJob",
-      schedule: "0 0 * * *" # Run daily at midnight (cron syntax)
-    }
-  ]
-end
